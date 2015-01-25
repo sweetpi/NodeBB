ad1c1a9a66e38cd527868ff2a4005c11340452a6
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-01-25 01:04:11 +0100

    Return votings of main topic in category

--- src/topics.js
+++ src/topics.js
@@ -168,6 +168,9 @@ var async = require('async'),
 				},
 				tags: function(next) {
 					Topics.getTopicsTagsObjects(tids, next);
+				},
+				mainPosts: function(next) {
+					Topics.getMainPosts(tids, uid, next);
 				}
 			}, function(err, results) {
 				if (err) {
@@ -176,6 +179,7 @@ var async = require('async'),
 
 				var users = _.object(uids, results.users);
 				var categories = _.object(cids, results.categories);
+				var mainPosts = _.object(tids, results.mainPosts);
 
 				for (var i=0; i<topics.length; ++i) {
 					if (topics[i]) {
@@ -183,7 +187,7 @@ var async = require('async'),
 						topics[i].user = users[topics[i].uid];
 						topics[i].teaser = results.teasers[i];
 						topics[i].tags = results.tags[i];
-
+						topics[i].votes = mainPosts[topics[i].mainPid].votes;
 						topics[i].isOwner = parseInt(topics[i].uid, 10) === parseInt(uid, 10);
 						topics[i].pinned = parseInt(topics[i].pinned, 10) === 1;
 						topics[i].locked = parseInt(topics[i].locked, 10) === 1;
