8f25e5bab8d698ee842a4514557e9f2f3b03caa1
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-01-25 12:12:19 +0100

    one more fix for votes

--- src/topics.js
+++ src/topics.js
@@ -187,8 +187,8 @@ var async = require('async'),
 						topics[i].user = users[topics[i].uid];
 						topics[i].teaser = results.teasers[i];
 						topics[i].tags = results.tags[i];
-
-						topics[i].votes = mainPosts[topics[i].tid].votes;
+						
+						topics[i].votes = (mainPosts[topics[i].tid] ? mainPosts[topics[i].tid].votes : '?');
 						topics[i].isOwner = parseInt(topics[i].uid, 10) === parseInt(uid, 10);
 						topics[i].pinned = parseInt(topics[i].pinned, 10) === 1;
 						topics[i].locked = parseInt(topics[i].locked, 10) === 1;
