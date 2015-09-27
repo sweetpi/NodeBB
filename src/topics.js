8a3fd02bb45b78c263483012425e017a5bb53cd2
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-09-27 11:32:29 +0200

    added vote count for topic

--- src/topics.js
+++ src/topics.js
@@ -178,12 +178,16 @@ var async = require('async'),
 					},
 					tags: function(next) {
 						Topics.getTopicsTagsObjects(tids, next);
-					}
+					},
+					mainPosts: function(next) {
+						Topics.getMainPosts(tids, uid, next);
+ 					}
 				}, next);
 			},
 			function(results, next) {
 				var users = _.object(uids, results.users);
 				var categories = _.object(cids, results.categories);
+				var mainPosts = _.object(tids, results.mainPosts);
 
 				for (var i=0; i<topics.length; ++i) {
 					if (topics[i]) {
@@ -191,6 +195,7 @@ var async = require('async'),
 						topics[i].user = users[topics[i].uid];
 						topics[i].teaser = results.teasers[i];
 						topics[i].tags = results.tags[i];
+						topics[i].votes = (mainPosts[topics[i].tid] ? mainPosts[topics[i].tid].votes : '?');
 
 						topics[i].isOwner = parseInt(topics[i].uid, 10) === parseInt(uid, 10);
 						topics[i].pinned = parseInt(topics[i].pinned, 10) === 1;
